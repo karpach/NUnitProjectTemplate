8fa4a0806b921fbe0c914820d910ee6e288a25cb
Viktar Karpach <vkarpach@comscore.com>
2014-03-05 10:15:13 -0600

    Initial commit

---
+++ Karpach.NUnitTemplate.Project/NUnitTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace $safeprojectname$
+{	
+    [TestFixture]
+    public class $itemname$
+    {		
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
+
